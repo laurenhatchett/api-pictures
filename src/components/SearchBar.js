2dfa64abede399e5dbad4d45a1d61f684ac5ab82
laurenhatchett <laurenhatchett@gmail.com>
2023-01-08 14:00:56 -0500

    create ImageList and ImageShow functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,4 @@
+import'./SearchBar.css'
 import { useState } from "react"
 
 function SearchBar ({ onSubmit }) {
@@ -6,7 +7,7 @@ function SearchBar ({ onSubmit }) {
   const handleFormSubmit = (event) => {
     event.preventDefault()
 
-    onSubmit('oceans')
+    onSubmit(term)
   }
 
   const handleChange = (event) => {
@@ -15,8 +16,9 @@ function SearchBar ({ onSubmit }) {
   }
 
   return (
-    <div> 
+    <div className='search-bar'> 
       <form onSubmit={handleFormSubmit}>
+        <label>Enter Search Term</label>
         <input value={term} onChange={handleChange}/>
       </form>
     </div>

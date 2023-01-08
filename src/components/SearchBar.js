f69e5bb97ebe160260a58df5b4acdbd12ea0c800
laurenhatchett <laurenhatchett@gmail.com>
2023-01-08 11:11:30 -0500

    create function to submit search turn using button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,11 @@
-function SearchBar () {
-  return <div> SearchBar </div>
+function SearchBar ({onSubmit}) {
+  const handleClick = () => {
+    onSubmit('oceans')
+  }
+  return <div> 
+      <input/>
+      <button onClick={handleClick}>Click me</button>
+    </div>
 }
 
 export default SearchBar

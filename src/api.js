6bdb46b95b1438d528bf216a69fede7aa960de23
laurenhatchett <laurenhatchett@gmail.com>
2023-01-07 19:31:17 -0500

    clean up searchImages function

--- src/api.js
+++ src/api.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
       Authorization: 'Client-ID SNrKJD-mu3kwWQ-9Bv6fDY436RkxRp_96ubnlslKqog'
 
     },
     params:{
-      query: 'oceans'
+      query: 'term'
 
     }
   })
 
-  return response
+  return response.data.results
 }
 
 export default searchImages
